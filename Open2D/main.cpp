d08de3e3b32a7fb944e880499b9c7a720e0cc4d6
junodeveloper <wnsgh224@naver.com>
2015-03-26 20:37:25 +0900

    Initial Commit

---
+++ Open2D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Open2D
+//
+//  Created by 황준호 on 2015. 3. 26..
+//  Copyright (c) 2015년 황준호. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
